6feb0818fd1e853a6013c041bfcf7291748fe653
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-09-20 19:59:35 +0530

    Update Expense-item.js

--- src/components/Expense-item.js
+++ src/components/Expense-item.js
@@ -1,1 +1,19 @@
-k
+import "./Expenseitem.css";
+
+function Expenseitem() {
+  const expenseDate= new Date(2021, 2, 21);
+  const expenseTitle='Car Insuranve';
+  const expenseAmount= 290;
+  
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Expenseitem;
