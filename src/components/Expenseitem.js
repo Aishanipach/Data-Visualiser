6a9e10f517bde9fa6b0c660828efae33cba091c9
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-09-21 20:11:55 +0530

    Update and rename Expense-item.js to Expenseitem.js

---
+++ src/components/Expenseitem.js
@@ -0,0 +1,16 @@
+import "./Expenseitem.css";
+import Expensedate from "./Expensedate";
+
+function Expenseitem(props) {
+  return (
+    <div className="expense-item">
+      <Expensedate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Expenseitem;
