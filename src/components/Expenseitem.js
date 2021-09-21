958a7611072e919f83a3e4e74bdaeca2be6c4293
Aishanii <59767187+Aishanipach@users.noreply.github.com>
2021-09-21 23:57:12 +0530

    Update Expenseitem.js

--- src/components/Expenseitem.js
+++ src/components/Expenseitem.js
@@ -1,15 +1,16 @@
 import "./Expenseitem.css";
 import Expensedate from "./Expensedate";
+import Card from "./Card";
 
 function Expenseitem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <Expensedate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
